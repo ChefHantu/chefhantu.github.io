914cddb03868c2f965159a91e340833d0bc2c1ad
ChefHantu <hafiz@chefhantu.com>
2026-01-31 20:19:56 +0800

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,40 @@
+// /assets/js/config.js
+window.CHEF_HANTU = {
+  name: "Chef Hantu",
+  url: "https://chefhantu.com",
+  emails: ["angah@chefhantu.com", "hafiz@chefhantu.com"],
+
+  phones: {
+    whatsappMY_display: "+60 11-5958 9600",
+    whatsappMY_e164: "601159589600",  // wa.me/601159589600
+  },
+
+  address: {
+    street: "55 Jalan Sentral 2, Taman Nusa Sentral",
+    city: "Gelang Patah",
+    region: "Johor",
+    postalCode: "79100",
+    country: "MY",
+    full: "55 Jalan Sentral 2, Taman Nusa Sentral, 79100 Gelang Patah, Johor"
+  },
+
+  hours: {
+    // pick ONE truth; everything renders from this
+    label: "Daily, 3:00 PM – 11:00 PM",
+    schema: "Mo-Su 15:00-23:00"
+  },
+
+  socials: {
+    instagram: "https://www.instagram.com/chefhantu/",
+    facebook: "https://www.facebook.com/profile.php?id=61582625955194",
+    tiktok: "https://www.tiktok.com/@chef_hantu"
+  },
+
+  images: {
+    hero: "https://chefhantu.com/images/hero.png",
+    og: "https://chefhantu.com/images/og-chef-hantu.jpg"
+  },
+
+  cuisine: ["Malaysian", "Western", "Fusion"],
+  priceRange: "$$"
+};
